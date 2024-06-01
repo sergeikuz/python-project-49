74bcec04bc69f1e5c07a90f20572c45d40f62136
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-06-01 23:26:42 +0300

    edit progression for work with engine

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,44 +1,18 @@
-import prompt
 import random
 
 
-def game_progression():
-    name = prompt.string(
-        "Welcome to the Brain Games!"
-        "\nMay I have your name? ")
-    print(
-        f"Hello, {name}!"
-        f"\nWhat number is missing in the progression?')")
+RULE = "What number is missing in the progression?')"
 
-    item = 0
-    while item < 3:
-        num1 = random.randint(1, 10)
-        num2 = random.randint(90, 120)
-        step = random.randint(1, 8)
-        list_of_num = []
 
-        for i in range(num1, num2, step):
-            list_of_num.append(i)
-
-        random_index = random.randint(0, 10)
-        correct_answer = list_of_num[random_index]
-        list_of_num[random_index] = ".."
-        question = " ".join(map(str, list_of_num[:11]))
-
-        answer = prompt.integer(
-            f"Question: {question}"
-            f"\nYour answer: ")
-
-        if answer == correct_answer:
-            print('Correct!')
-            item += 1
-
-        else:
-            print(
-                f"'{answer}' is wrong answer ;(."
-                f" Correct answer was '{correct_answer}'."
-                f"\nLet's try again, {name}!")
-            item = 4
-
-        if item == 3:
-            print(f"Congratulations, {name}!")
+def game_conditions():
+    num1 = random.randint(1, 10)
+    num2 = random.randint(90, 120)
+    step = random.randint(1, 8)
+    list_of_num = []
+    for i in range(num1, num2, step):
+        list_of_num.append(i)
+    random_index = random.randint(0, 10)
+    correct_answer = str(list_of_num[random_index])
+    list_of_num[random_index] = ".."
+    question = " ".join(map(str, list_of_num[:11]))
+    return question, correct_answer
