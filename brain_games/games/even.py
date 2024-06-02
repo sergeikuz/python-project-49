03d513e31a23e3b351a2b273924243167c03a9f1
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-06-02 20:10:10 +0300

    add func: is_even to even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,11 +4,16 @@ from random import randint
 RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def game_conditions():
-    number = randint(1, 999)
+def is_even(number):
     if number % 2 == 0:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
+    return correct_answer
+
+
+def game_conditions():
+    number = randint(1, 999)
+    correct_answer = is_even(number)
     question = f"{number}"
     return question, correct_answer
