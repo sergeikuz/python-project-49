7c92f6ae18d868150603415f50f120882ed8abf1
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-06-02 18:54:12 +0300

    try to fix E126 continuation line over-indented for hanging indent

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,8 +8,8 @@ def engine_for_brain_games(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(
-            f"Hello, {name}!"
-            f"\n{game.RULE}")
+        f"Hello, {name}!"
+        f"\n{game.RULE}")
     item = 0
     while item < MAX_ROUNDS:
         question, correct_answer = game.game_conditions()
@@ -20,9 +20,9 @@ def engine_for_brain_games(game):
             item += 1
         else:
             print(
-                    f"'{answer}' is wrong answer ;(. "
-                    f"Correct answer was '{correct_answer}'."
-                    f"\nLet's try again, {name}!")
+                f"'{answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'."
+                f"\nLet's try again, {name}!")
             break
         if item == MAX_ROUNDS:
             print(f"Congratulations, {name}!")
