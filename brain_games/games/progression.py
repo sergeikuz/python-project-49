4c7cc3c68933c6ac73324d8acc6871521c6cd0d2
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-06-02 19:15:09 +0300

    edit progression: add func for list

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,14 +4,19 @@ import random
 RULE = "What number is missing in the progression?"
 
 
-def game_conditions():
+def generator_list_of_num():
     num1 = random.randint(1, 10)
     num2 = random.randint(90, 120)
     step = random.randint(1, 8)
     list_of_num = []
     for i in range(num1, num2, step):
         list_of_num.append(i)
+    return list_of_num
+
+
+def game_conditions():
     random_index = random.randint(0, 10)
+    list_of_num = generator_list_of_num()
     correct_answer = str(list_of_num[random_index])
     list_of_num[random_index] = ".."
     question = " ".join(map(str, list_of_num[:11]))
