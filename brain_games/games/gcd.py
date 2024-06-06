fc8d1248fc8f94199fba3d16d5b9ea16790f0731
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-06-06 17:30:11 +0300

    refuctoring module gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,12 +4,7 @@ from random import randint
 RULE = "Find the greatest common divisor of given numbers."
 
 
-def get_conditions():
-    number1 = randint(1, 100)
-    number2 = randint(1, 100)
-
-    question = f"{number1} {number2}"
-
+def get_greatest_divisor(number1, number2):
     while number1 != 0 and number2 != 0:
         if number1 > number2:
             number1 = number1 % number2
@@ -17,6 +12,16 @@ def get_conditions():
             number2 = number2 % number1
 
     answer = number1 + number2
+    return answer
+
+
+def get_conditions():
+    number1 = randint(1, 100)
+    number2 = randint(1, 100)
+
+    question = f"{number1} {number2}"
+
+    answer = get_greatest_divisor(number1, number2)
     correct_answer = str(answer)
 
     return question, correct_answer
